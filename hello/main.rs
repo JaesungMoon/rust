bf9905ea926553167ec62f6e493bf1789f93a430
jason <jsdekiru@gmail.com>
2025-04-29 15:10:40 +0900

    ch2

--- hello/main.rs
+++ hello/main.rs
@@ -7,4 +7,22 @@ fn main() {
 
     // print!("First line\nSecond line\nThird line\n");
 
+    print!("The sum is {}", 80.3 + 34.9);
+
+    println!("{}", "These
+        are
+        three lines");
+    
+    println!("{}", "This \
+        is \
+        just one line");
+
+    println!("{}", "These
+are
+three lines");
+
+println!("{}", "These\n\
+    are\n\
+    three lines");
+
 }
