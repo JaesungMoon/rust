17d1224881fad56a8f560a77ba75486dd2031d2c
jason <jsdekiru@gmail.com>
2025-04-29 13:29:49 +0900

    ch1

---
+++ hello/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    // print!("{}, {}", "Hello", "world!--");
+
+    // print!("{}, {}!", "Hello");
+
+    // print!("First line\nSecond line\nThird line\n");
+
+}
