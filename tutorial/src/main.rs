e61ee95cc20cbabe06e76068f3ad37ff758a913b
jason <jsdekiru@gmail.com>
2025-05-02 18:32:53 +0900

    trait

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -8,12 +8,35 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 
 fn main() {
-    struct Rectangle<T, U> {
-        length: T,
-        height: U,
+    const PI: f32 = 3.141592;
+
+    trait Shape {
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
     }
-    let rec = Rectangle {
-        length: 4,
-        height: 1.5
-    };
+    struct Rectangle { length: f32, width: f32 };
+    struct Circle { length: f32 };
+
+    impl Shape for Rectangle {
+        fn new(length: f32, width: f32) -> Rectangle {
+            return Rectangle{length, width};
+        }
+        fn area(&self) -> f32 {
+            return self.length * self.width;
+        }
+    }
+    impl Shape for Circle {
+        fn new(length: f32, width: f32) -> Circle {
+            return Circle{length};
+        }
+        fn area(&self) -> f32 {
+            return (self.length / 2.0).powf(2.0) * PI;
+        }
+    }
+    let rec: Rectangle = Shape::new(10.0, 5.0);
+    let cir: Circle = Shape::new(10.0, 10.0);
+
+    println!("Rectangle area: {}", rec.area());
+    println!("Circle ared: {}", cir.area());
+
 }
